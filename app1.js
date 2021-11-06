8b308182089d756b941b19504105665d6d7fbf59
Prabhat <prabhatmotwani752@gmail.com>
2021-11-06 09:42:35 +0530

    commiting project

---
+++ app1.js
@@ -0,0 +1,37 @@
+//Movement Animation to happen
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+//Items
+const title = document.querySelector(".title");
+const racket = document.querySelector(".racket img");
+const purchase = document.querySelector(".purchase");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+
+//Moving Animation Event
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+//Animate In
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  //Popout
+  title.style.transform = "translateZ(150px)";
+  racket.style.transform = " rotateZ(-25deg)";
+  description.style.transform = "translateZ(100px)";
+  sizes.style.transform = "translateZ(100px)";
+  purchase.style.transform = "translateZ(100px)";
+});
+//Animate Out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  //Popback
+  title.style.transform = "translateZ(0px)";
+  racket.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
+});
